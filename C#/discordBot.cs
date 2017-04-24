5876cb3e9cbf65a71a2b7c1bf3d81eaf32d0a867
TheForgotten5 <meteor.97@hotmail.com>
2017-04-23 23:40:10 -0700

    Uploaded the main() for the discord bot.

---
+++ C#/discordBot.cs
@@ -0,0 +1,18 @@
+using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace discordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Discord_Bot bot = new Discord_Bot();
+        }
+    }
+}
